80340bdabd84344fd6e07b70663c1f39cd13b126
flores-97 <floresvictor15@gmail.com>
2025-01-24 17:09:03 -0800

    created project file

--- COMP003A.LectureActivity3.6/Program.cs
+++ COMP003A.LectureActivity3.6/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace COMP003A.LectureActivity3._6
+﻿/*
+ Author: Victor Flores
+Course:COMP-003A
+Faculty: Jonathon Cruz
+Purpose: Lecture Activity 3.6*/
+namespace COMP003A.LectureActivity3._6
 {
     internal class Program
     {
