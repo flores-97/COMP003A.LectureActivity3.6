4605c9b202f0a18e8c997f5a6edaff3348d57fd8
flores-97 <floresvictor15@gmail.com>
2025-01-24 18:39:22 -0800

    first section replicated

--- COMP003A.LectureActivity3.6/Program.cs
+++ COMP003A.LectureActivity3.6/Program.cs
@@ -9,7 +9,30 @@ namespace COMP003A.LectureActivity3._6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Advanced Control Statements Demo!");
+
+            int range; //range of nums to iterate through
+            //get range of numbers to iterate through
+            while (true)
+                {
+                //try catch block handle invalid input
+                try
+                {
+                    //prompt use to enter pos integer
+                    Console.Write("Enter a positive integer to iterate throug: ");
+                    //parse the input to an integer
+                    range = int.Parse(Console.ReadLine());
+                    //check if num pos
+                    if (range <= 0) throw new Exception("Number must be positive.");
+                    //exit loop if input is valid
+                    break;
+                }
+                catch (Exception ex)
+                { //display an error message and prompt use to try again
+                  Console.WriteLine($"Invalid input: {ex.Message}\nPlease try again.\n");
+                }
+                
+            }
         }
     }
 }
